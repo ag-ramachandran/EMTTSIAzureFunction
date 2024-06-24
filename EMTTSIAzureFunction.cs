714e02dea41f8bdd7cd45512b6c58a2ac4425e10
ramacg <ramacg@microsoft.com>
2024-06-24 14:59:29 +0530

    *  Add comments

--- EMTTSIAzureFunction.cs
+++ EMTTSIAzureFunction.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
@@ -14,6 +13,11 @@ using System.Linq;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Fabric.TSI
 {
+    /**
+        * This function is used to get the aggregated values of the PI Points from the TSI database.
+        * The function takes in a list of tags, startDateTime, endDateTime and binInterval as input and returns the aggregated values of the PI Points.
+        * The function uses the Kusto binding to query the Kusto database and get the aggregated values of the PI Points. This uses the declarative function call method to invoke the Kusto query
+    */
     public class GetAggregates
     {
         [FunctionName("GetPIPointValuesTSI")]
@@ -28,7 +32,11 @@ namespace Microsoft.Azure.WebJobs.Extensions.Fabric.TSI
             var startDateTime = tsiQueryRequest.startDateTime.ToString("o", System.Globalization.CultureInfo.InvariantCulture);
             var endDateTime = tsiQueryRequest.endDateTime.ToString("o", System.Globalization.CultureInfo.InvariantCulture);
             var binInterval = tsiQueryRequest.binInterval;
-            var kustoAttribute = new KustoAttribute("PI-TimeSeriesData-For-EMT")
+
+            /*
+                Uses the Fabric KQL binding to query the Kusto database and get the aggregated values of the PI Points.
+            */
+            var kustoAttribute = new KustoAttribute("PI-TimeSeriesData-For-EMT") // Database name
             {
                 KqlCommand = "declare query_parameters (tags:string,startDate:datetime,endDate:datetime,timebucket:timespan);GetTagAggregates(tags,startDate,endDate,timebucket)",
                 KqlParameters = $"@tags={tags},@startDate={startDateTime},@endDate={endDateTime},@timebucket={binInterval}",
@@ -40,11 +48,8 @@ namespace Microsoft.Azure.WebJobs.Extensions.Fabric.TSI
     }
     public record TSIQueryRequest(List<string> tags, DateTime startDateTime, DateTime endDateTime, string binInterval);
     public record GetPIPointValuesTSI(JsonArray data);
-
     public record AggregateValue(string timestamp, double value);
-
     public record AggregateValues(string tagName, List<AggregateValue> values);
-
     public record AggregateValuesWrapper(List<AggregateValues> data);
 
 
